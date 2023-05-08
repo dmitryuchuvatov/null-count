5bd6e72d2b3639ac445bc6fe9b2e90243c3e1b90
Dmitry Uchuvatov <dmitry.uchuvatov@hashicorp.com>
2023-05-08 11:28:21 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,14 @@
+terraform {
+  required_providers {
+    null = {
+      source = "hashicorp/null"
+      version = "3.2.1"
+    }
+  }
+}
+
+provider "null" {}
+
 resource "null_resource" "test" {
   count = 2
 
